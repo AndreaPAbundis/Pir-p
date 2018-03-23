0e95e5585136f727b6fcacf0c12236052695a898
AndreaPAbundis <lizbethabundispgmail.com>
2018-03-23 14:09:42 -0600

    WIP-Andrea
    home

--- index.php
+++ index.php
@@ -9,8 +9,9 @@
   <link rel="icon" type="image/png" href="https://dummyimage.com/180x180/000/fff" sizes="180x180">
   <!-- [#TODO] Dont forget to update the FAVICON IMAGES in the next route -->
   <!-- <link rel="icon" type="image/png" href="assets/images/favicon-180x180.png" sizes="128x128"> -->
-  <link href="https://cdnjs.cloudflare.com/ajax/libs/normalize/4.2.0/normalize.min.css" integrity="sha256-K3Njjl2oe0gjRteXwX01fQD5fkk9JFFBdUHy/h38ggY=" crossorigin="anonymous" rel="stylesheet">
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/normalize/4.2.0/normalize.min.css" integrity="sha256-K3Njjl2oe0gjRteXwX01fQD5fkk9JFFBdUHy/h28ggY=" crossorigin="anonymous" rel="stylesheet">
   <link href="./assets/styles/main.css" rel="stylesheet">
+  <link rel="stylesheet" href="assets/swiper/swiper/dist/css/swiper.min.css">
 </head>
 
 <body>
@@ -48,25 +49,177 @@
     </header>
   </div>
   <!--  Hero -->
-  <div class="hero-image">
+  <!--<div class="hero-image">
     <div class="hero-text">
       <h1 style="font-size:50px">I am John Doe</h1>
       <p>And I'm a Photographer</p>
       <button class="ripple">Hire me</button>
     </div>
-  </div>
+  </div>-->
+  <!--Slider-->
+<?php include('Slider.php'); ?>
   <!--  Main content -->
   <main class="j-workspace ">
     <div class="j-wrap">
-      
+      <div class="quienes-content">
+        <div class="description">
+          <h1>¿Quienes somos?</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        </div>
+        <div class="img-content">
+          <div class="thumbnail-img-content">
+            <div class="image-container">
+              <img src="https://dummyimage.com/400x250/000/fff" alt="">
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="content-all-service-pre">
+      <h1 class="title">Servicios</h1>
+      <div class="s-empresarial">
+        <div class="thumbnail-img-content">
+          <div class="image-container">
+            <img src="https://dummyimage.com/400x250/000/fff" alt="">
+          </div>
+        </div>
+        <div class="description-ser">
+          <h2>Empresarial</h2>
+          <button>Ver más</button>
+        </div>
+      </div>
+      <div class="s-social">
+        <div class="thumbnail-img-content">
+          <div class="image-container">
+            <img src="https://dummyimage.com/400x250/000/fff" alt="">
+          </div>
+        </div>
+        <div class="description-ser">
+          <h2>Social</h2>
+          <button>Ver más</button>
+        </div>
+      </div>
+      <div class="s-patronal">
+        <div class="thumbnail-img-content">
+          <div class="image-container">
+            <img src="https://dummyimage.com/400x250/000/fff" alt="">
+          </div>
+        </div>
+        <div class="description-ser">
+          <h2>Patronal</h2>
+          <button>Ver más</button>
+        </div>
+      </div>
     </div>
   </main>
+  <main class="j-workspace ">
+    <div class="j-wrap">
+      <div class="listing-products">
+        <h1>Productos</h1>
+        <div class="p-Crisantemos">
+          <div class="thumbnail-img-content">
+            <div class="image-container">
+              <img src="https://dummyimage.com/400x250/000/fff" alt="">
+            </div>
+          </div>
+          <div class="description-p">
+            <h2>Crisantemos</h2>
+            <button>Ver más</button>
+          </div>
+        </div>
+        <div class="p-Crisantemos">
+          <div class="thumbnail-img-content">
+            <div class="image-container">
+              <img src="https://dummyimage.com/400x250/000/fff" alt="">
+            </div>
+          </div>
+          <div class="description-p">
+            <h2>Crisantemos</h2>
+            <button>Ver más</button>
+          </div>
+        </div>
+        <div class="p-minytra">
+          <div class="thumbnail-img-content">
+            <div class="image-container">
+              <img src="https://dummyimage.com/400x250/000/fff" alt="">
+            </div>
+          </div>
+          <div class="description-p">
+            <h2>Minas y trazadores</h2>
+            <button>Ver más</button>
+          </div>
+        </div>
+        <div class="p-multitiros">
+          <div class="thumbnail-img-content">
+            <div class="image-container">
+              <img src="https://dummyimage.com/400x250/000/fff" alt="">
+            </div>
+          </div>
+          <div class="description-p">
+            <h2>Multitiros</h2>
+            <button>Ver más</button>
+          </div>
+        </div>
+        <div class="p-logosyl">
+          <div class="thumbnail-img-content">
+            <div class="image-container">
+              <img src="https://dummyimage.com/400x250/000/fff" alt="">
+            </div>
+          </div>
+          <div class="description-p">
+            <h2>Loogos y leyendas</h2>
+            <button>Ver más</button>
+          </div>
+        </div>
+        <div class="p-castillos">
+          <div class="thumbnail-img-content">
+            <div class="image-container">
+              <img src="https://dummyimage.com/400x250/000/fff" alt="">
+            </div>
+          </div>
+          <div class="description-p">
+            <h2>Castillos</h2>
+            <button>Ver más</button>
+          </div>
+        </div>
+        <div class="p-pirfria">
+          <div class="thumbnail-img-content">
+            <div class="image-container">
+              <img src="https://dummyimage.com/400x250/000/fff" alt="">
+            </div>
+          </div>
+          <div class="description-p">
+            <h2>Pirotecnia fria</h2>
+            <button>Ver más</button>
+          </div>
+        </div>
+      </div>
+    <div>
+  </main>
   <footer>
+    <div class="j-wrap">
+      <div class="container-footer">
+        <div class="contact-ways">
+          <img src="https://dummyimage.com/200x50/000/fff">
+          <div class="datos-dir">
+            <h2>Dirección</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing.</p>
+          </div>
+          <div class="redes-logos">
+            <img class="thumbnail-im" src="assets/images/Logos/logo-facebook.png">
+            <img class="thumbnail-im" src="assets/images/Logos/logo-google.png">
+            <img src="assets/images/Logos/logo-insta.png">
+            <div>
+        </div>
+        <div class="cl-left"></div>
+      </div>
+    </div>
   </footer>
 
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
   <script src="assets/scripts/ripple.js"></script>
   <script src="assets/scripts/header.js"></script>
-
+  <script src="assets/swiper/swiper/dist/js/swiper.min.js"></script>
 </body>
 </html>
