77ec5f66ba48977117f2e3c2ae779350d70fe791
Andrea_Abundis <andrea.abundis@aori.mx>
2018-03-23 00:34:28 -0600

    WIP-Andrea
    home

---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width,initial-scale=1">
+  <title>P-Palma</title>
+  <link rel="icon" type="image/png" href="https://dummyimage.com/64x64/000/fff" sizes="64x64">
+  <link rel="icon" type="image/png" href="https://dummyimage.com/180x180/000/fff" sizes="180x180">
+  <!-- [#TODO] Dont forget to update the FAVICON IMAGES in the next route -->
+  <!-- <link rel="icon" type="image/png" href="assets/images/favicon-180x180.png" sizes="128x128"> -->
+  <link href="https://cdnjs.cloudflare.com/ajax/libs/normalize/4.2.0/normalize.min.css" integrity="sha256-K3Njjl2oe0gjRteXwX01fQD5fkk9JFFBdUHy/h38ggY=" crossorigin="anonymous" rel="stylesheet">
+  <link href="./assets/styles/main.css" rel="stylesheet">
+</head>
+
+<body>
+  <!--  Header  -->
+
+  <div class="header-container">
+    <header class="main-nav">
+      <nav role="navigation" class="navbar">
+        <ul>
+          <a href="">
+            <li class="space-options"><img src="https://dummyimage.com/200x50/000/fff"></li>
+          </a>
+          <a href="">
+            <li class="space-options">Nosotros |</li>
+          </a>
+          <a href="">
+            <li class="space-options">Servicios |</li>
+          </a>
+          <a href="">
+            <li class="space-options">Eventos |</li>
+          </a>
+          <a href="">
+            <li class="space-options">Productos |</li>
+          </a>
+          <a href="">
+            <li class="space-options">Contacto |</li>
+          </a>
+        </ul>
+      </nav>
+      <div id="nav-icon">
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+    </header>
+  </div>
+  <!--  Hero -->
+  <div class="hero-image">
+    <div class="hero-text">
+      <h1 style="font-size:50px">I am John Doe</h1>
+      <p>And I'm a Photographer</p>
+      <button class="ripple">Hire me</button>
+    </div>
+  </div>
+  <!--  Main content -->
+  <main class="j-workspace ">
+    <div class="j-wrap">
+      
+    </div>
+  </main>
+  <footer>
+  </footer>
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+  <script src="assets/scripts/ripple.js"></script>
+  <script src="assets/scripts/header.js"></script>
+
+</body>
+</html>
