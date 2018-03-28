69e66e7ea0c3adb034d2f429c984aa50f6d93af5
AndreaPAbundis <lizbethabundispgmail.com>
2018-03-28 13:06:42 -0600

    WIP-Andrea
    nosotros

--- nosotros.php
+++ nosotros.php
@@ -76,6 +76,44 @@
       </div>
     </div>
   </main>
+  <div class="slogan-content">
+    <h1>Slogan</h1>
+    <p>Lorem ipsum dolor sit amet</p>
+  </div>
+  <main class="j-workspace ">
+    <div class="j-wrap">
+        <div class="proposito-content">
+          <div class="img-proposito">
+            <div class="thumbnail-img-content">
+              <div class="image-container">
+                <img src="https://dummyimage.com/400x250/000/fff" alt="">
+              </div>
+            </div>
+         </div>
+          <div class="description-pro">
+            <h2>Nuestro proposito</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </div>
+        </div>
+        <div class="mercado-content">
+          <div class="description-mer">
+          <h2>Mercado</h2>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+          </div>
+          <div class="img-mer">
+            <div class="thumbnail-img-content">
+              <div class="image-container">
+                <img src="https://dummyimage.com/400x250/000/fff" alt="">
+              </div>
+            </div>
+         </div>
+        </div>
+    </div>
+  </main>
+  <div class="content-others">
+    <h1>Lo que nos diferencia</h1>
+    <p>Lorem ipsum dolor sit amet</p>
+  </div>
   <footer>
     <div class="j-wrap">
       <div class="container-footer">
